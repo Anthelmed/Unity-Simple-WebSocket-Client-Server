daf37c499a2e945db32c1dfd88b3dcaa4836a696
anthelmed <anthelme.dumont@edu.gobelins.fr>
2019-05-09 16:16:03 +0200

    update server broadcast method

--- Assets/Server/server.js
+++ Assets/Server/server.js
@@ -11,7 +11,13 @@ wss.on('connection', function connection(ws) {
         console.log('disconnected');
     });
 
-    ws.on('message', function incoming(message) {
-        ws.send(message);
+    ws.on('message', function incoming(data) {
+        console.log("message received");
+
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
     });
 });
